20dd4a4a42cdacb1a7efe02d6e3706e5d40b695a
manug <manugarg2@gmail.com>
2017-05-24 22:28:10 +0800

    multi threaded account transfer

---
+++ src/main/java/extras/accounts/CreditDebitTransactions.java
@@ -0,0 +1,64 @@
+package extras.accounts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.String.format;
+
+public class CreditDebitTransactions {
+    private final Map<Integer, Account> accounts;
+
+    public CreditDebitTransactions() {
+        accounts = new HashMap<>();
+    }
+
+    public CreditDebitTransactions withAccount(int acNo, double initBalance) {
+        if (accounts.containsKey(acNo))
+            throw new RuntimeException(
+                    format("account '%s' already initialized with balance '%s'", acNo, initBalance));
+
+        this.accounts.put(acNo, new Account(acNo, initBalance));
+        return this;
+    }
+
+    public boolean transfer(int from, int to, double amount) {
+        if (! accounts.containsKey(from))
+            return false;
+        final Account f = accounts.get(from);
+
+        if (! accounts.containsKey(to))
+            return false;
+        final Account t = accounts.get(to);
+
+        return _transfer(amount, f, t);
+    }
+
+    private boolean _transfer(double amount, Account f, Account t) {
+        if (f.balance < amount)
+            return false;
+
+        f.balance -= amount;
+        t.balance += amount;
+
+        return true;
+    }
+
+    public double balance(int acNo) {
+        if (accounts.containsKey(acNo))
+            return accounts.get(acNo).balance;
+        else
+            return 0.0;
+    }
+
+}
+
+class Account {
+    final int acNo;
+    double balance;
+
+    Account(int acNo, double balance) {
+        this.acNo = acNo;
+        this.balance = balance;
+    }
+
+}
---
+++ src/test/java/extras/accounts/TestCreditDebitTransactions.java
@@ -0,0 +1,34 @@
+package extras.accounts;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class TestCreditDebitTransactions {
+
+    @Test public void should_not_transfer_when_low_balance() {
+        manager
+                .withAccount(10001, 100.00)
+                .withAccount(10002, 123.00);
+
+
+        assertFalse(manager.transfer(10001, 10002, 1000.00));
+
+        assertEquals(100.0, manager.balance(10001), 0.0001);
+        assertEquals(123.0, manager.balance(10002), 0.0001);
+    }
+
+    final CreditDebitTransactions manager = new CreditDebitTransactions();
+    @Test public void should_transfer_from_one_account_to_another() {
+        manager
+                .withAccount(10001, 100.00)
+                .withAccount(10002, 123.00);
+
+        manager.transfer(10001, 10002, 10.00);
+        assertEquals(90.0, manager.balance(10001), 0.0001);
+        assertEquals(133.0, manager.balance(10002), 0.0001);
+    }
+
+}
