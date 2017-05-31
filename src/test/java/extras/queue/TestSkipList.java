01e26e241c66ab1ecd0e994daca6e821944ebfb1
manug <manugarg2@gmail.com>
2017-05-31 08:03:35 +0800

    starting work for min priority queue using skip list

--- src/test/java/extras/queue/TestSkipList.java
+++ src/test/java/extras/queue/TestSkipList.java
@@ -14,6 +14,21 @@ public class TestSkipList {
         assertEquals(200, sl.extract_min());
     }
 
+    @Test public void should_be_able_to_add_and_extract() {
+        sl.add(200);
+        sl.add(100);
+        assertEquals(100, sl.extract_min());
+        sl.add(100);
+        assertEquals(100, sl.extract_min());
+        assertEquals(200, sl.extract_min());
+        sl.add(500);
+        sl.add(300);
+        sl.add(400);
+        assertEquals(300, sl.extract_min());
+        assertEquals(400, sl.extract_min());
+        assertEquals(500, sl.extract_min());
+    }
+
     @Test public void should_be_able_to_extract_min_2_times() {
         sl.add(100);
         sl.add(200);
